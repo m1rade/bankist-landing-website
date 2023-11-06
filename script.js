5522d4b1c3f846126fbd9e065cd29cb77def223f
m1rade <forx98@gmail.com>
2023-11-07 01:02:36 +0300

    lazy loading images

--- script.js
+++ script.js
@@ -149,7 +149,6 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
     const [entry] = entries;
-    console.log(entry);
 
     if (!entry.isIntersecting) return;
 
@@ -160,12 +159,36 @@ const revealSection = function (entries, observer) {
 
 const sectionsObs = new IntersectionObserver(revealSection, {
     root: null,
-    threshold: 0.3,
+    threshold: 0.1,
 });
 allSections.forEach(section => {
     sectionsObs.observe(section);
     section.classList.add('section--hidden');
 });
+
+///////////////////////////////////////
+////* Lazy loading */
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.src = entry.target.dataset.src;
+    entry.target.addEventListener('load', function () {
+        entry.target.classList.remove('lazy-img');
+    });
+
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    threshold: 0,
+    root: null,
+    rootMargin: '200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ////////////////////////////////////////////////////////////////////
 ///////* Selecting Elements */
